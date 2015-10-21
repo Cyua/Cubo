4e23c1c77603ed914accc748102d4c05b0330ad2
FrozenShore <frozenshore@FrozenShore.local>
2015-10-21 10:57:40 +0800

    完成脚本建模
    可通过脚本进行建模，并使用对应Prefebs，保存到对应模版下

--- raw/Assets/scripts/CubeController.cs
+++ raw/Assets/scripts/CubeController.cs
@@ -2,19 +2,60 @@
 using System.Collections;
 
 public class CubeController : MonoBehaviour {
-
+	public GameObject instantiate;
 	public float rotate_speed;
 
 	private float rotate_vertical;
 	private float rotate_horizontal;
 	private Quaternion initial_rotation;
+	private GameObject cubes;
 
 	public void resetCubes(){
 		transform.rotation = initial_rotation;
 	}
 
+	int returnMiddle(int x){
+		double tempf = x * 1.0 / 2;
+		int tempi = x / 2;
+		if (tempf > tempi)
+			return tempi+1;
+		else
+			return tempi;
+	}
+
 	void Start () {
 		initial_rotation = transform.rotation;
+		int[,,] shape = new int[,,] {
+			{{0,0,0,0,0}, {0,1,0,1,0}, {0,0,0,0,0}},
+			{{0,1,1,1,0}, {1,1,1,1,1}, {0,1,1,1,0}},
+			{{0,1,1,1,0}, {1,1,1,1,1}, {0,1,1,1,0}},
+			{{0,0,1,0,0}, {0,1,1,1,0}, {0,0,1,0,0}},
+			{{0,0,0,0,0}, {0,0,1,0,0}, {0,0,0,0,0}}
+		};
+		int l = shape.GetLength(2);    // 5
+		int w = shape.GetLength(1);    // 3
+		int h = shape.GetLength(0);    // 5
+
+		int mx = returnMiddle (l)-1;  //calculate the middle point
+		int my = returnMiddle (h);
+		int mz = returnMiddle (w)-1;
+
+		float px = transform.position.x;  //parent's position
+		float py = transform.position.y;
+		float pz = transform.position.z;
+
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				for (int k = 0; k < l; k++) {
+					if (shape[i,j,k] == 1) {
+						cubes = Instantiate (instantiate, transform.position, transform.rotation) as GameObject;
+						cubes.transform.SetParent(this.transform,true);
+						cubes.transform.position = new Vector3(k-mx+px, h-i-my+py, j-mz+pz);
+					}
+				}
+			}
+		}
+
 	}
 
 	void Update () {
--- raw/Assets/scripts/SingleCube.cs
+++ raw/Assets/scripts/SingleCube.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 
 public class SingleCube : MonoBehaviour {
-	public Material afterClick;
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
