7a284f28811c714287167329191894350b72faa0
FrozenShore <frozenshore@FrozenShore.local>
2015-10-21 11:14:28 +0800

    改善操作模式
    现在是选中小方块后，再按一下空格才消除方块。也就是说可以同时选中多个方块一起消除。
    现在急需实现选中方块变色功能

--- raw/Assets/scripts/SingleCube.cs
+++ raw/Assets/scripts/SingleCube.cs
@@ -2,18 +2,20 @@
 using System.Collections;
 
 public class SingleCube : MonoBehaviour {
-
+	bool isClicked;
 	// Use this for initialization
 	void Start () {
-
+		isClicked = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		if (isClicked && Input.GetKeyDown ("space")) {
+			gameObject.SetActive(false);
+		}
 	}
 
 	void OnMouseDown(){
-		gameObject.SetActive(false);
+		isClicked = !isClicked;
 	}
 }
