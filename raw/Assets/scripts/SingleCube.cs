94f84ecaf921fcb261a4b31c7683bc1e2d05761d
ZHW <18868119485@163.com>
2015-10-22 23:22:38 +0800

    可以改变颜色了

--- raw/Assets/scripts/SingleCube.cs
+++ raw/Assets/scripts/SingleCube.cs
@@ -2,20 +2,30 @@
 using System.Collections;
 
 public class SingleCube : MonoBehaviour {
-	bool isClicked;
+	private bool isClicked;
+	private Renderer render;
+	public Material originalMat;
+	public Material newMat;
+
 	// Use this for initialization
 	void Start () {
 		isClicked = false;
+		render = GetComponent<Renderer> (); 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (isClicked && Input.GetKeyDown ("space")) {
+		if (isClicked && Input.GetKeyDown("space")) {
 			gameObject.SetActive(false);
 		}
 	}
 
 	void OnMouseDown(){
 		isClicked = !isClicked;
+		if (isClicked) {
+			render.material = newMat;
+		} else {
+			render.material = originalMat;
+		}
 	}
 }
