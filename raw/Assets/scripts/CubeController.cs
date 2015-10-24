5ac2f1d9d6e356386ce23881750a52949ee7fa87
FrozenShore <Cyua@FrozenShore.local>
2015-10-24 23:05:25 +0800

    实现资源文件的分离
    现在每个关卡的初始cube建模放在Resources目录下，例如第一关的坐标值可以存放为level1.txt

--- raw/Assets/scripts/CubeController.cs
+++ raw/Assets/scripts/CubeController.cs
@@ -5,9 +5,11 @@ public class CubeController : MonoBehaviour {
 	public GameObject instantiate;
 	public float rotate_speed;
 
-	private float rotate_vertical;
-	private float rotate_horizontal;
-	private Quaternion initial_rotation;
+	private float rotate_vertical;			//rotate cubes
+	private float rotate_horizontal;		//rotate cubes
+	private Quaternion initial_rotation;	//record the initial rotation
+	private ArrayList rawInput = new ArrayList();
+	private int[,,] shape;
 	private GameObject cubes;
 
 	public void resetCubes(){
@@ -23,15 +25,42 @@ public class CubeController : MonoBehaviour {
 			return tempi;
 	}
 
+	void genShape(string target){
+		TextAsset level = (TextAsset)Resources.Load (target);
+		string temp = level.text;
+		int h = 0;
+		int w = 0;
+		int l = 0;
+		string[] first = temp.Split ('\n');			//fill the rawInput
+		h = first.Length;
+		foreach (string x in first) {
+			string[] second = x.Split(',');
+			w = second.Length;
+			foreach(string y in second){
+				string[] third = y.Split(' ');
+				l = third.Length;
+				foreach(string z in third){
+					int node = int.Parse(z);
+					rawInput.Add(node);
+				}
+			}
+		}
+		int count = 0;
+		shape = new int[h, w, l];
+		for (int i = 0; i < h; i++) {
+			for(int j = 0; j < w; j++){
+				for(int k = 0; k < l; k++){
+					shape[i,j,k] = (int)rawInput[count++];
+				}
+			}
+		}
+
+	}
+
 	void Start () {
 		initial_rotation = transform.rotation;
-		int[,,] shape = new int[,,] {
-			{{0,0,0,0,0}, {0,1,0,1,0}, {0,0,0,0,0}},
-			{{0,1,1,1,0}, {1,1,1,1,1}, {0,1,1,1,0}},
-			{{0,1,1,1,0}, {1,1,1,1,1}, {0,1,1,1,0}},
-			{{0,0,1,0,0}, {0,1,1,1,0}, {0,0,1,0,0}},
-			{{0,0,0,0,0}, {0,0,1,0,0}, {0,0,0,0,0}}
-		};
+		genShape ("level1");
+
 		int l = shape.GetLength(2);    // 5
 		int w = shape.GetLength(1);    // 3
 		int h = shape.GetLength(0);    // 5
