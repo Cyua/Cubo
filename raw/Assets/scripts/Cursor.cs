e531c8f6fa9ad95f08cec63883579edf5259edd8
dmxcsnsbh <l.dmxcsnsbh@gmail.com>
2015-11-05 11:27:49 +0800

    Cursor Added

---
+++ raw/Assets/scripts/Cursor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor : MonoBehaviour {
+	private bool isClick = false;
+
+	public Texture mouseTexture;
+	public Texture mouseTextureClicked;
+	// Use this for initialization
+	void Start () {
+		UnityEngine.Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			if (Input.GetKey ("space"))
+				isClick = true;
+			else
+				isClick = false;
+		} else {
+			isClick = false;
+		}
+	}
+
+	void OnGUI()
+	{
+		if (isClick) {
+			Vector3 mousePos = Input.mousePosition;
+			GUI.DrawTexture (new Rect (mousePos.x, Screen.height - mousePos.y, 
+			                           mouseTexture.width, mouseTexture.height), mouseTextureClicked);
+		} else {
+			Vector3 mousePos = Input.mousePosition;
+			GUI.DrawTexture (new Rect (mousePos.x, Screen.height - mousePos.y, 
+			                           mouseTexture.width, mouseTexture.height), mouseTexture);
+		}
+	}
+}
